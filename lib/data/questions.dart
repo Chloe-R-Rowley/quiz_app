09110b66c832e9ba759c6927b36943473ae1647a
Chloe R. Rowley <chloeruthrowley2002@gmail.com>
2023-08-20 14:56:02 +0530

    added a data model and dummy data

---
+++ lib/data/questions.dart
@@ -0,0 +1,55 @@
+import 'package:quiz_app/models/question.dart';
+
+const questions = [
+  QuizQuestion(
+    'How many bones does the human body have?',
+    [
+      '206',
+      '126',
+      '502',
+      '205',
+    ],
+  ),
+  QuizQuestion('What is the powerhouse of a cell?', [
+    'mitochondria',
+    'nucleus',
+    'ribosomes',
+    'lysosome',
+  ]),
+  QuizQuestion(
+    'Which protein is responsible for transporting oxygen in our blood?',
+    [
+      'haemoglobin',
+      'keratin',
+      'collagen',
+      'myoglobin',
+    ],
+  ),
+  QuizQuestion(
+    'Which is the rarest blood group?',
+    [
+      'AB negative',
+      'B negative',
+      'O negative',
+      'AB positive',
+    ],
+  ),
+  QuizQuestion(
+    'How many chromosomes are in the human gene?',
+    [
+      '46',
+      '48',
+      '23',
+      '24',
+    ],
+  ),
+  QuizQuestion(
+    'Which of the following acid is present in Vinegar?',
+    [
+      'Acetic acid',
+      'Formic acid',
+      'Citric acid',
+      'Oxalic acid',
+    ],
+  ),
+];
